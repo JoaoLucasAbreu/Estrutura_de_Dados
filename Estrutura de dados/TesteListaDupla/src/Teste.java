91aabc35b006e8dc84f111915a975dd86b72eb7f
João Lucas Abreu <78802497+JoaoLucasAbreu@users.noreply.github.com>
2022-03-18 09:58:36 -0300

    Upload

---
+++ Estrutura de dados/TesteListaDupla/src/Teste.java
@@ -0,0 +1,14 @@
+package TesteListaDupla.src;
+
+import ListaDupla.src.Lista;
+
+public class Teste {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        Lista lista = new Lista();
+        lista.Inserir("nome", 123);
+        lista.Imprimir();
+        
+    }
+}
