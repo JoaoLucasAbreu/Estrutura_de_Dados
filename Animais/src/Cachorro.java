fa42db7b5a0eaf20caabc53d235b9ab86144c630
João Lucas Abreu <78802497+JoaoLucasAbreu@users.noreply.github.com>
2022-03-18 09:47:05 -0300

    Erase

--- Animais/src/Cachorro.java
+++
@@ -1,6 +0,0 @@
-public class Cachorro {
-
-    public static void main(String[] args) {
-        System.out.println("Macaco");
-    }
-}
--- Atv_1/src/Aluno.java
+++
@@ -1,6 +0,0 @@
-public class Aluno {
-
-    public static void main(String[] args) {
-        System.out.println("Macaco");
-    }
-}
